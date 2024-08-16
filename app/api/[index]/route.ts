15f014d820fecfcea8dbfda74b7e53b488f63511
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2024-08-16 06:28:55 +0530

    Build: Add Basic Details

--- app/api/[index]/route.ts
+++ app/api/[index]/route.ts
@@ -5,6 +5,7 @@ import { promisify } from "util";
 import redis from "redis";
 import { CasheSingleton } from "@/app/database/data/CasheSingleton";
 import ExistingStudent from "@/app/models/e_student";
+import { StudentType } from "@/app/utl/studenttype";
 
 interface Params {
   index: string;
@@ -34,6 +35,33 @@ export async function GET(req: NextRequest, { params }: { params: Params }) {
       });
     }
     return NextResponse.json({ studentType: "New Student" });
+  } catch (err: any) {
+    return NextResponse.json({ error: err.message });
+  }
+}
+
+export async function POST(req: NextRequest, { params }: { params: Params }) {
+  const { index } = params;
+
+  try {
+    await dbConnect();
+
+    const studentData = await req.json();
+
+    if (!studentData) {
+      return NextResponse.json({ error: "No data found" });
+    }
+
+    const newStudent = new ExistingStudent();
+    newStudent.olindexno = index;
+    if (await StudentType(index)) {
+      newStudent.oldclass = studentData;
+    } else {
+      newStudent.oldSchool = studentData;
+    }
+
+    const result = await newStudent.save(); // This assumes you're using Mongoose
+    return NextResponse.json({ message: "Data added successfully", result });
   } catch (err: any) {
     return NextResponse.json({ error: err.message });
   }
