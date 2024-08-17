a87f12073b12853ed208a72c5f99349e3bcb0f5e
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2024-08-17 20:30:07 +0530

    Build: Added boilerplate styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main>
-      <h1>Hi! I am Dinujaya</h1>
-    </main>
-  );
+  return <main className="main"></main>;
 }
