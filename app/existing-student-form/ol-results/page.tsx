176fd1e82254ac6fc2eaad1b176435554bef0e33
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2024-09-07 18:29:56 +0530

    Build: Created a attempt selector for O/L results

--- app/existing-student-form/ol-results/page.tsx
+++ app/existing-student-form/ol-results/page.tsx
@@ -3,6 +3,15 @@ import React from "react";
 const page = () => {
   return (
     <>
+      <div className="ol-attempts-select">
+        <h3>This is my,</h3>
+        <select name="">
+          <option value="1">First attempt </option>
+          <option value="2">Second attempt</option>
+          <option value="3">Third attempt</option>
+        </select>
+        <h3>for O/L</h3>
+      </div>
       <fieldset className="fieldSet">
         <legend>O/L Results - First Attempt</legend>
       </fieldset>
