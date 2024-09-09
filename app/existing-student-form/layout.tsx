51b9573d1b454d3e05210036401b7ce7f94f28bd
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2024-09-09 19:21:59 +0530

    Build: Instruction form & banner

--- app/existing-student-form/layout.tsx
+++ app/existing-student-form/layout.tsx
@@ -2,6 +2,8 @@
 
 import React from "react";
 import Stepper from "awesome-react-stepper";
+import Image from "next/image";
+import banner from "@images/bannerTwo.jpg";
 
 const Layout = ({
   children,
@@ -11,6 +13,19 @@ const Layout = ({
   return (
     <>
       <main className="wrapper">
+        <div className="banner">
+          <Image
+            src={banner}
+            alt="background"
+            className="banner-image"
+            style={{ width: "100%", height: "auto" }}
+            // objectFit="cover"
+          />
+          <div className="textTitle">
+            <h4 className="heading">A/L Application Form</h4>
+            <h4 className="subHeading">Mayurapada Central College Narammala</h4>
+          </div>
+        </div>
         <div className="exisitingStudents">
           <h1 className="mainTitle">Apply as an Existing Student</h1>
           {children}
